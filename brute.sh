4c10ce65becbabebc99246ed3f936ee14eda844d
bossa96demo <ilyaershov09@gmail.com>
2023-06-14 20:42:03 +0300

    bash compatible script added

---
+++ brute.sh
@@ -0,0 +1,10 @@
+if (test $# -lt 1); then
+	read -p "first part of website (ex. abob): " site
+else
+	site=$argv[1]
+fi
+for i in $(cat ./tlds-alpha-by-domain.txt); do 
+	if (curl --head --silent $site.$i > /dev/null 2>&1); then
+		echo $site.$i exists
+	fi
+done
