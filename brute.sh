0069daa741498f834a9de7ffbb301b15fc6f6d4e
bossa96demo <ilyaershov09@gmail.com>
2023-06-14 22:31:58 +0300

    error with accepting args fixed

--- brute.sh
+++ brute.sh
@@ -1,7 +1,7 @@
 if (test $# -lt 1); then
 	read -p "first part of website (ex. abob): " site
 else
-	site=$argv[1]
+	site=$1
 fi
 for i in $(cat ./tlds-alpha-by-domain.txt); do 
 	if (curl --head --silent $site.$i > /dev/null 2>&1); then
